c3597a129a08ec2fe56bc0f6a21cfbb552684b7a
luzi82 <luzi82@gmail.com>
2022-05-25 13:16:17 +0800

    init code

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: 'assets/',
+        to:'assets/'
+      }]
+    }),
+    new HtmlWebpackPlugin({
+      title: 'codelog.pixijs',
+    }),
+  ],
+  mode: 'development',
+  entry: {
+    main: './main.js',
+  },
+  output: {
+    filename: 'cp-[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'source-map'
+};
