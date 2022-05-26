5730cf9aab2c31753aadc9029a0f9a31b7a6030b
luzi82 <luzi82@gmail.com>
2022-05-26 05:53:02 +0000

    add sokoban base

---
+++ src/js/game/sokoban/sokoban.js
@@ -0,0 +1,24 @@
+// import Sokoban from '../../game/sokoban/sokoban.js';
+
+import * as lz_game_object from '../../lz/lz_game_object.js';
+import * as PIXI from 'pixi.js';
+
+'use strict';
+
+export const Sokoban = (function(){
+
+class Sokoban extends lz_game_object.LzGameObject {
+
+  constructor(runtime){
+    super(runtime);
+    
+    
+  };
+
+};
+
+return Sokoban;
+
+})();
+
+export default Sokoban;
